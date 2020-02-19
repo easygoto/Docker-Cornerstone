18e7dbfe8fde67b1abdb3a89d5d5e88a1c776bf3
trink <treelink@126.com>
2020-02-19 15:02:36 +0800

    - `fixed` 修复 docker 命令 restart 的错误

--- run.bat
+++ run.bat
@@ -6,7 +6,7 @@
     @%deployCmd% start
 ) else (
     @if "%1" == "re" (
-        @%deployCmd% %2 %3 %4 %5 %6 %7 %8 %9
+        @%deployCmd% restart %2 %3 %4 %5 %6 %7 %8 %9
     ) else (
         @%deployCmd% %*
     )
